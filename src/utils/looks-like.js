68440ac5daa4c9fa8765ada9c8cdc01690769648
Matt Phillips <matt@mattphillips.io>
2018-02-01 21:37:49 +0000

    Add looks like function

---
+++ src/utils/looks-like.js
@@ -0,0 +1,20 @@
+function looksLike(a, b) {
+  return (
+    a &&
+    b &&
+    Object.keys(b).every(bKey => {
+      const bVal = b[bKey];
+      const aVal = a[bKey];
+      if (typeof bVal === 'function') {
+        return bVal(aVal);
+      }
+      return isPrimitive(bVal) ? bVal === aVal : looksLike(aVal, bVal);
+    })
+  );
+}
+
+function isPrimitive(val) {
+  return val == null || /^[sbn]/.test(typeof val);
+}
+
+module.exports = looksLike;
